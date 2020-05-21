fd8ee2cb2c0ea00cd252ffb717ff8416cdb0fa37
govilharsh <govil@iitg.ac.in>
2020-05-21 18:04:26 +0530

    signup page with name

--- cabsharing/register/views.py
+++ cabsharing/register/views.py
@@ -1,17 +1,19 @@
 from django.contrib.auth import login, authenticate
-from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
+
+from register.forms import SignUpForm
+
 
 def index(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = SignUpForm(request.POST)
         if form.is_valid():
             form.save()
-            username=form.cleaned_data.get('username')
-            raw_password=form.cleaned_data.get('password1')
-            user=authenticate(username=username, password=raw_password)
-            login(request,user)
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
             return redirect('home')
     else:
-        form=UserCreationForm()
-    return render(request,'register/index.html',{'form' : form})
+        form = SignUpForm()
+    return render(request, 'register/index.html', {'form': form})
